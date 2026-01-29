7c73bd4bec75f2b4c099a34b27d5656854d5b398
cbid71 <biardcyrille@gmail.com>
2026-01-30 00:47:15 +0100

    chore!: require google provider >= 6.15.0

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.26.0, < 8"
+      version = ">= 6.15.0, < 8"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.26.0, < 8"
+      version = ">= 6.15.0, < 8"
     }
   }
 
